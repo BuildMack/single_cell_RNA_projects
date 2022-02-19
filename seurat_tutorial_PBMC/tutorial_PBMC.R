d169a78c46b1f35772b408be89a5c43b2fc3f21d
Mack Sell <m.m.sell@protonmail.com>
2022-02-18 22:38:19 -0500

    initial commit

---
+++ seurat_tutorial_PBMC/tutorial_PBMC.R
@@ -0,0 +1,16 @@
+#Seurat Tutorial - Peripheral Blood Mononuclear Cells (PBMCs)
+#Date: Feb. 18th 2022
+#Completed by: Mack Sell
+#Description:
+
+#dplyr is a new package which provides a set of tools for efficiently manipulating datasets in R
+library(dplyr)
+
+library(Seurat)
+
+#The goal of patchwork is to make it ridiculously simple to combine separate ggplots into the same graphic.
+#ggplot2 is an open-source data visualization package for the statistical programming language R.
+library(patchwork)
+
+
+
